69ca9b6f1a5cb8a16f4df55154c601d6c3b35bfb
Gla3nata <0207natalya@gmail.com>
2025-02-07 23:43:16 +0600

    lesson02Task1

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "study-glo";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 484930;
+let rollback = 13;
+let fullPrice = 1000;
+let adaptive = true;
 
 alert("Привет мир");
-console.log("Это тестовое сообщение в консоли");
+console.log(title, fullPrice, adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов" + screenPrice +" долларов " + "Стоимость разработки сайта " +fullPrice +" долларов" );
+console.log(screens.toLowerCase());
+console.log(fullPrice * (rollback/100));
